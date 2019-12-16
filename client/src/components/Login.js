eba9bd9eeab748906669e873b0bf75eba87effc3
CodingCCarpenter <christineassists@aol.com>
2019-12-15 20:28:01 -0600

    hashing out login component

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,48 @@
 import React from "react";
+import useForm from 'react-hook-form';
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+import axiosWithAuth from '../auth/axiosWithAuth';
+
+const Login = (props) => {
+  const { register, handleSubmit, reset } = useForm();
+  const onSubmit = (data) => {
+    axiosWithAuth()
+      // make a post request to retrieve a token from the api
+      .post('/login', data)
+      .then((res) => {
+        localStorage.setItem('token', res.data.payload);
+        props.history.push('/dash')
+      })
+      .catch((err) => {
+        console.log(
+          'src/components/Login.js: onSubmit: axiosWithAuth: .catch ERROR: ', 
+          err
+        )
+      });
+      // when you have handled the token, navigate to the BubblePage route
+  }
+  
   return (
-    <>
+    <div>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+      {/* <p>Build a login page here</p> */}
+      <form onSubmit= { handleSubmit(onSubmit) }>
+        <label for='username'>
+          Username: 
+        </label>
+        <input type='text' name='username' id='username' ref={ register } />
+        <br/>
+
+        <label for='password'>
+          Password: 
+        </label>
+        <input type='password' name='password' id='password' ref={ register } />
+        <br/>
+        
+        <input type='submit' value='Log In' />
+      </form>
+
+    </div>
   );
 };
 
